f7afbffda59e91fc3c85f0395d055042bfc8ec06
Pablo Derendinger <98769613+pderendinger-everse@users.noreply.github.com>
2024-07-25 17:09:07 -0300

    hotfix for demo

--- GLTF2BIM/GLTF.Extensions.BIM/Containers/glTFBIMPropertyContainer.cs
+++ GLTF2BIM/GLTF.Extensions.BIM/Containers/glTFBIMPropertyContainer.cs
@@ -5,16 +5,18 @@ using Newtonsoft.Json;
 
 using GLTF2BIM.GLTF.Extensions.BIM.BaseTypes;
 
-namespace GLTF2BIM.GLTF.Extensions.BIM.Containers {
+namespace GLTF2BIM.GLTF.Extensions.BIM.Containers
+{
     [Serializable]
-    public class glTFBIMPropertyContainer : glTFBIMContainer {
-        string _uri;
-        glTFBIMPropertyData _propData = new glTFBIMPropertyData();
+    public class glTFBIMPropertyContainer : glTFBIMContainer
+    {
+        private string _uri;
+        private glTFBIMPropertyData _propData = new glTFBIMPropertyData();
 
-        public glTFBIMPropertyContainer(string uri) {
+        public glTFBIMPropertyContainer(string uri)
+        {
             _uri = uri;
         }
-
 
         [JsonProperty("$type")]
         public override string Type => "properties";
@@ -22,21 +24,24 @@ namespace GLTF2BIM.GLTF.Extensions.BIM.Containers {
         [JsonProperty("uri")]
         public override string Uri => _uri;
 
-        public bool HasPropertyData => _propData.Records.Count > 0;
+        public bool HasPropertyData = true;
 
         public void Record(string id, Dictionary<string, object> props) => _propData.Record(id, props);
 
-        public string Pack() {
+        public string Pack()
+        {
             return JsonConvert.SerializeObject(
                     _propData,
-                    new JsonSerializerSettings {
+                    new JsonSerializerSettings
+                    {
                         NullValueHandling = NullValueHandling.Ignore
                     }
                 );
         }
 
         [Serializable]
-        class glTFBIMPropertyData {
+        private class glTFBIMPropertyData
+        {
             [JsonProperty("records", Order = 1)]
             public Dictionary<string, HashSet<uint>> Records { get; set; } = new Dictionary<string, HashSet<uint>>();
 
@@ -49,28 +54,34 @@ namespace GLTF2BIM.GLTF.Extensions.BIM.Containers {
             [JsonProperty("values", Order = 4)]
             public List<object> Values { get; set; } = new List<object>();
 
-            public void Record(string id, Dictionary<string, object> props) {
+            public void Record(string id, Dictionary<string, object> props)
+            {
                 // add properties and group
                 var grp = new glTFBIMPropertyDataGroup();
 
-                foreach (var propData in props) {
+                foreach (var propData in props)
+                {
                     if (propData.Value is null)
                         continue;
 
                     // add key
-                    if (Keys.IndexOf(propData.Key) is int keyIdx && keyIdx != -1) {
+                    if (Keys.IndexOf(propData.Key) is int keyIdx && keyIdx != -1)
+                    {
                         grp.Keys.Add((uint)keyIdx);
                     }
-                    else {
+                    else
+                    {
                         Keys.Add(propData.Key);
                         grp.Keys.Add((uint)Keys.Count - 1);
                     }
 
                     // add value
-                    if (Values.IndexOf(propData.Value) is int valueIdx && valueIdx != -1) {
+                    if (Values.IndexOf(propData.Value) is int valueIdx && valueIdx != -1)
+                    {
                         grp.Values.Add((uint)valueIdx);
                     }
-                    else {
+                    else
+                    {
                         Values.Add(propData.Value);
                         grp.Values.Add((uint)Values.Count - 1);
                     }
@@ -79,29 +90,34 @@ namespace GLTF2BIM.GLTF.Extensions.BIM.Containers {
                 uint groupIndex = 0;
                 if (Groups.IndexOf(grp) is int grpIndex && grpIndex > -1)
                     groupIndex = (uint)grpIndex;
-                else {
+                else
+                {
                     Groups.Add(grp);
                     groupIndex = (uint)Groups.Count - 1;
                 }
 
-                if (Records.TryGetValue(id, out var groupIndices)) {
+                if (Records.TryGetValue(id, out var groupIndices))
+                {
                     groupIndices.Add(groupIndex);
                 }
-                else {
+                else
+                {
                     Records.Add(id, new HashSet<uint> { groupIndex });
                 }
             }
         }
 
         [Serializable]
-        class glTFBIMPropertyDataGroup {
+        private class glTFBIMPropertyDataGroup
+        {
             [JsonProperty("keys", Order = 1)]
             public List<uint> Keys { get; set; } = new List<uint>();
 
             [JsonProperty("values", Order = 2)]
             public List<uint> Values { get; set; } = new List<uint>();
 
-            public override bool Equals(object obj) {
+            public override bool Equals(object obj)
+            {
                 if (obj is glTFBIMPropertyDataGroup other)
                     return Keys.Equals(other.Keys) && Values.Equals(other.Values);
                 return false;
